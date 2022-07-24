f3dc4256cd675691b55d4409651b3f500118c6fa
Vivek Kumar Rai <vivekkumar.rai@publicissapient.com>
2022-07-24 23:40:18 +0530

    tomcat

--- authentication/src/main/java/com/tweet/app/SpringSecurityProApplication.java
+++ authentication/src/main/java/com/tweet/app/SpringSecurityProApplication.java
@@ -2,6 +2,7 @@ package com.tweet.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,7 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
-public class SpringSecurityProApplication {
+public class SpringSecurityProApplication extends SpringBootServletInitializer {
 
 
 	public static void main(String[] args) {
